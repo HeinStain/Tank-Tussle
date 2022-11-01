2c7df56903fc39174a4202a383ea79afc50e548c
HeinStain <99003140+HeinStain@users.noreply.github.com>
2022-11-01 19:20:49 -0400

    Framework Built

---
+++ Tank Tussle/Assets/SceneLoader.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    
+
+    ////TEMPORARY////
+    public Canvas gameOverCanvas;
+    public Canvas gameCanvas;
+
+    //Load Level (Called by various buttons)
+    public void LoadLevel(string targetScene)
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene(targetScene);
+    }
+
+    //Quit Game (Only called by quit button)
+    public void Quit()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+
+
+    ////TEMPORARY////
+    public void OpenGameOverScreen()
+    {  
+        gameCanvas.enabled = false;
+        gameOverCanvas.enabled = true;
+    }
+}
